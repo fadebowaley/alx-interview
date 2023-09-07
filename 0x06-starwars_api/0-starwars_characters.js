a762b09a684b0eeb0d2d95c64d49a6e50dced522
Ademola Adebowale <fadebowaley@gmail.com>
2023-09-07 03:06:26 +0100

    starwars API request calls

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,49 @@
-#!/usr/bin/node
-// script that prints all characters of a Star Wars movie:
+#!/usr/bin/env node
+
 const request = require("request");
+
+// Check if a movie ID is provided as a command-line argument
+if (process.argv.length !== 3) {
+  console.error("Usage: ./0-starwars_characters.js <Movie ID>");
+  process.exit(1);
+}
+
 const movieId = process.argv[2];
-const options = {
-  url: "https://swapi-api.alx-tools.com/api/films/" + movieId,
-  method: "GET",
-};
+// Define the Star Wars API URL for fetching movie data
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(options, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
-
-function printCharacters(characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+// Function to fetch and display characters
+function fetchCharacters(url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error("Error:", error);
+      process.exit(1);
     }
+    if (response.statusCode !== 200) {
+      console.error("API Error. Status code:", response.statusCode);
+      process.exit(1);
+    }
+
+    const movieData = JSON.parse(body);
+
+    // Display characters
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error("Error:", charError);
+          process.exit(1);
+        }
+        if (charResponse.statusCode !== 200) {
+          console.error("API Error. Status code:", charResponse.statusCode);
+          process.exit(1);
+        }
+
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
+      });
+    });
   });
 }
+
+// Fetch characters for the specified movie
+fetchCharacters(apiUrl);
